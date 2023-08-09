7974288c2185a5a8bcbc4a9870ef8777c43ea79a
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-09 20:10:50 +0530

    Create a child component for Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,11 @@
+import Tiles from './ButtonsUI';
+
 const Calculator = () => (
   <div className="calculator-outer">
     <div className="calculator-input">
       <input type="text" placeholder="0" id="variable" required />
     </div>
+    <Tiles />
   </div>
 );
 
