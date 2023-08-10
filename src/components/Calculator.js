410814e4d353672cc12f93ec12917fb6c9745a09
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-10 20:07:50 +0530

    Import useState and set the initial state and also the update

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,44 @@
-const Calculator = () => (
-  <div className="calculator-outer">
-    <div className="calculator-input">
-      0
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  const clickEvent = (e) => {
+    const TILEVALUE = e.target.innerText;
+    const RESULT = calculate(state, TILEVALUE);
+    setState(RESULT);
+  };
+  const { total, operation, next } = state;
+  return (
+    <div className="calculator-outer">
+      <div className="calculator-input">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="calculator-buttons">
+        <button className="tiles" type="button" onClick={clickEvent}>AC</button>
+        <button className="tiles" type="button" onClick={clickEvent}>+/-</button>
+        <button className="tiles" type="button" onClick={clickEvent}>%</button>
+        <button className="tiles operations" type="button" onClick={clickEvent}>÷</button>
+        <button className="tiles" type="button" onClick={clickEvent}>7</button>
+        <button className="tiles" type="button" onClick={clickEvent}>8</button>
+        <button className="tiles" type="button" onClick={clickEvent}>9</button>
+        <button className="tiles operations" type="button" onClick={clickEvent}>x</button>
+        <button className="tiles" type="button" onClick={clickEvent}>4</button>
+        <button className="tiles" type="button" onClick={clickEvent}>5</button>
+        <button className="tiles" type="button" onClick={clickEvent}>6</button>
+        <button className="tiles operations" type="button" onClick={clickEvent}>-</button>
+        <button className="tiles" type="button" onClick={clickEvent}>1</button>
+        <button className="tiles" type="button" onClick={clickEvent}>2</button>
+        <button className="tiles" type="button" onClick={clickEvent}>3</button>
+        <button className="tiles operations" type="button" onClick={clickEvent}>+</button>
+        <button className="tiles" type="button" onClick={clickEvent}>0</button>
+        <button className="tiles" type="button" onClick={clickEvent}>.</button>
+        <button className="tiles operations" type="button" onClick={clickEvent}>=</button>
+      </div>
     </div>
-    <div className="calculator-buttons">
-      <button className="tiles" type="button">AC</button>
-      <button className="tiles" type="button">+/-</button>
-      <button className="tiles" type="button">%</button>
-      <button className="tiles operations" type="button">÷</button>
-      <button className="tiles" type="button">7</button>
-      <button className="tiles" type="button">8</button>
-      <button className="tiles" type="button">9</button>
-      <button className="tiles operations" type="button">x</button>
-      <button className="tiles" type="button">4</button>
-      <button className="tiles" type="button">5</button>
-      <button className="tiles" type="button">6</button>
-      <button className="tiles operations" type="button">-</button>
-      <button className="tiles" type="button">1</button>
-      <button className="tiles" type="button">2</button>
-      <button className="tiles" type="button">3</button>
-      <button className="tiles operations" type="button">+</button>
-      <button className="tiles" type="button">0</button>
-      <button className="tiles" type="button">.</button>
-      <button className="tiles operations" type="button">=</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
