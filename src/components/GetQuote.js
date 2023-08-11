1fbdf6b58713680fb5e061e37cf1d878b767e6d6
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-11 17:07:38 +0530

    Fetch api data and display in the DOM

--- src/components/GetQuote.js
+++ src/components/GetQuote.js
@@ -1,8 +1,33 @@
-const Quote = () => (
-  <div className="quote-display">
-    <p className="quote">Test printing</p>
-    <p className="author">author</p>
-  </div>
-);
+import { useEffect, useState } from 'react';
+
+const Quote = () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=courage', {
+        method: 'GET',
+        withCredentials: true,
+        headers: {
+          'X-Api-Key': 'uCHBJSjC0mhJo8VJsKMGKw==TE5WVQkHClEOg2cP',
+          'Content-Type': 'application/json',
+        },
+      });
+      const json = await res.json();
+      setData(json[0]);
+    };
+    fetchData();
+  }, [setData]);
+  return (
+    <div className="quote-display">
+      <p className="quote">
+        {data.quote}
+      </p>
+      <p className="author">
+        {data.author}
+      </p>
+    </div>
+  );
+};
 
 export default Quote;
