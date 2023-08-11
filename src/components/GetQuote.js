0192227cc639713adb3f613af4f9e3a1c58e587f
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-11 16:44:05 +0530

    Test for quote component display

--- src/components/GetQuote.js
+++ src/components/GetQuote.js
@@ -1,3 +1,8 @@
-const Quote = () => {
-    
-}
+const Quote = () => (
+  <div className="quote-display">
+    <p className="quote">Test printing</p>
+    <p className="author">author</p>
+  </div>
+);
+
+export default Quote;
