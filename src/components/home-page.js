8ac7da7462f465d19352ad99d2b782e6b7bded79
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-17 03:27:43 +0530

    Create a module for the home page JSX

---
+++ src/components/home-page.js
@@ -0,0 +1,57 @@
+import '../styles/homeStyles.css';
+
+const Home = () => (
+  <div className="home-outer">
+    <h1 className="home-heading">Welcome to Math Magicians! There is a Calculator Next door😁</h1>
+    <div className="home-inner">
+      <h2>Interesting Facts</h2>
+      <div className="parts">
+        <div className="part-one">
+          <p>
+            The closest living relative to humans are chimpanzees, bonobos,
+            and gorillas. We share between 98 and 99.6% of DNA with these species.
+            Gorillas can even catch colds from humans.
+          </p>
+          <p>
+            Most mammals have reproductive cycles. However, only humans,
+            humpback whales, and elephants experience menopause.
+          </p>
+          <p>To escape a crocodiles jaw, push your thumb into its eyeballs.</p>
+          <p>
+            Cats have only lived with people for about 7,000 years. Compared to
+            dogs, whose domestication may have begun as early as 25,000 years ago.
+          </p>
+          <p>Dog noses are as unique as a human fingerprint.</p>
+          <p>
+            Many animals exhibit high levels of emotional intelligence.
+            For example, cows form bonds akin to friendships, and often have a &#34;best
+            friend,&#34; and Gentoo Penguins bring a potential mate a pebble to &#34;propose.&#34;
+          </p>
+          <p>
+            The first drive-in service station in the United States was opened by Gulf Oil
+            Company - on December 1, 1913, in Pittsburgh, Pennsylvania.
+          </p>
+          <p>
+            Clans of long ago that wanted to get rid of unwanted people without
+            killing them used to burn their houses down --
+            hence the expression &#34;to get fired&#34;
+          </p>
+          <p>
+            The citrus soda 7-UP was created in 1929; &#34;7&#34; was selected
+            because the original containers were 7 ounces. &#34;UP&#34;
+            indicated the direction of the bubbles.
+          </p>
+          <p>Elephants are the only mammals that can&#39;t jump and Elephants have four knees.</p>
+          <p>
+            Each of the suits on a deck of cards represents the four major pillars of the
+            economy in the middle ages: heart represented the Church, spades represented
+            the military, clubs represented agriculture, and diamonds
+            represented the merchant class.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Home;
