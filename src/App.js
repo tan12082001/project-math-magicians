556fb254eeb901bc97d6f5efdb8513c85f16e82b
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-17 03:30:25 +0530

    modify App.js with React router package for SPA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/GetQuote';
+import Navbar from './components/nav-bar';
+import Home from './components/home-page';
 
 function App() {
   return (
-    <>
-      <Quote />
-      <Calculator />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </Router>
   );
 }
 
